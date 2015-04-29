ba3068fc20f2712b7dffce03d7ff06320236c425
James Foster <englandjf@yahoo.com>
2015-04-29 11:32:25 -0700

    Working on check boxes and saving them to the db

--- app/src/main/java/com/ssu/jnn/cs470final/preferences.java
+++ app/src/main/java/com/ssu/jnn/cs470final/preferences.java
@@ -7,14 +7,45 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 
 public class preferences extends ActionBarActivity {
+
+    CheckBox allBoxes[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+        //Store all checkboxes in array
+        allBoxes = new CheckBox[13];
+        allBoxes[0] = (CheckBox)findViewById(R.id.checkBox);
+        allBoxes[1] = (CheckBox)findViewById(R.id.checkBox2);
+        allBoxes[2] = (CheckBox)findViewById(R.id.checkBox3);
+        allBoxes[3] = (CheckBox)findViewById(R.id.checkBox4);
+        allBoxes[4] = (CheckBox)findViewById(R.id.checkBox5);
+        allBoxes[5] = (CheckBox)findViewById(R.id.checkBox6);
+        allBoxes[6] = (CheckBox)findViewById(R.id.checkBox7);
+        allBoxes[7] = (CheckBox)findViewById(R.id.checkBox8);
+        allBoxes[8] = (CheckBox)findViewById(R.id.checkBox9);
+        allBoxes[9] = (CheckBox)findViewById(R.id.checkBox10);
+        allBoxes[10] = (CheckBox)findViewById(R.id.checkBox11);
+        allBoxes[11] = (CheckBox)findViewById(R.id.checkBox12);
+        allBoxes[12] = (CheckBox)findViewById(R.id.checkBox13);
+
+
+
+
     }
 
 
@@ -47,6 +78,30 @@ public class preferences extends ActionBarActivity {
                 Intent signupIntent = new Intent(new Intent(preferences.this, signUp.class));
                 startActivity(signupIntent);
                 break;
+            case R.id.updatePrefs:
+                Log.i("Update","Prefs");
+                final boolean temp[] = new boolean[13];
+                for(int i = 0; i < 13; i++) {
+                    temp[i] = allBoxes[i].isChecked();
+                }
+                //Puts array in db
+                /*
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+                query.whereEqualTo("username","I am nick");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> userList, ParseException e) {
+                        if (e == null) {
+                            userList.get(0).put("currentInterests",temp);
+                            userList.get(0).saveInBackground();
+
+                        } else {
+                            Log.d("score", "Error: " + e.getMessage());
+                        }
+                    }
+                });
+                */
+
+                //break;
         }
     }
 
