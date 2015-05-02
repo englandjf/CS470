85e9e5aca9c3c0750183f1e9b3770dbb67e21af3
Nick Mahnke <mahnke.nick@gmail.com>
2015-05-01 22:44:22 -0700

    Added data and time pickers for addMarker.

--- app/src/main/java/com/ssu/jnn/cs470final/addMarker.java
+++ app/src/main/java/com/ssu/jnn/cs470final/addMarker.java
@@ -1,31 +1,36 @@
 package com.ssu.jnn.cs470final;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
 
-public class addMarker extends ActionBarActivity {
+public class addMarker extends ActionBarActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +58,6 @@ public class addMarker extends ActionBarActivity {
         categoriesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(categoriesArrayAdapter);
 
-
     }
 
 
@@ -79,8 +83,58 @@ public class addMarker extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public static class TimePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), (addMarker)getActivity(), hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), (addMarker)getActivity(), year, month, day);
+        }
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        TextView dateText = (TextView)findViewById(R.id.dateText);
+        dateText.setText(month + "/" + day + "/" + year);
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView timeText = (TextView)findViewById(R.id.timeText);
+        timeText.setText(hourOfDay + ":" + minute);
+    }
+
+    public void OnDateSelect(View v){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void OnTimeSelect(View v){
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
     public void ButtonOnClick(View v){
-        EditText placeNameField = (EditText)findViewById(R.id.pinName);
+        EditText placeNameField = (EditText)findViewById(R.id.markerName);
         Spinner categoryField = (Spinner)findViewById(R.id.categories);
         EditText addressField = (EditText)findViewById(R.id.address);
         EditText descriptionField = (EditText)findViewById(R.id.description);
@@ -97,8 +151,7 @@ public class addMarker extends ActionBarActivity {
             e.printStackTrace();
         }
         Address a = addresses.get(0);
-        Log.wtf("address", a.toString());
-        if(addresses.size() > 0) {
+        if(a != null) {
             latitude = a.getLatitude();
             longitude = a.getLongitude();
         }
